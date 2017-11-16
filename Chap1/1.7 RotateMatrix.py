0c5e04e8f1a5165e98dfde029f5876aaff681d71
HarryFray <harry.fray7@gmail.com>
2017-11-16 10:20:07 -0800

    code works but at what cost? need to study Big O

---
+++ Chap1/1.7 RotateMatrix.py
@@ -0,0 +1,57 @@
+''''''
+''' my code rotates matrix ccw and does not work for the test case
+should understand and simplify line 9 covaluted s'''
+def rotate_matrix(matrix):
+    emptymatrix = []
+    for ii in range(len(matrix)):
+        emptymatrix.append([])
+        for row in matrix:
+            emptymatrix[ii].append(row[-(ii-(len(matrix)-1))])
+    return emptymatrix
+
+
+
+
+
+
+
+
+
+max = [
+
+            [1, 2, 3, 4, 5],
+            [6, 7, 8, 9, 10],
+            [11, 12, 13, 14, 15],
+            [16, 17, 18, 19, 20],
+            [21, 22, 23, 24, 25]
+        ]
+
+print rotate_matrix(max)
+
+'''
+class Test(unittest.TestCase):
+    # Test Cases
+    data = [
+        ([
+            [1, 2, 3, 4, 5],
+            [6, 7, 8, 9, 10],
+            [11, 12, 13, 14, 15],
+            [16, 17, 18, 19, 20],
+            [21, 22, 23, 24, 25]
+        ], [
+            [21, 16, 11, 6, 1],
+            [22, 17, 12, 7, 2],
+            [23, 18, 13, 8, 3],
+            [24, 19, 14, 9, 4],
+            [25, 20, 15, 10, 5]
+        ])
+    ]
+
+    def test_rotate_matrix(self):
+        for [test_matrix, expected] in self.data:
+            actual = rotate_matrix(test_matrix)
+            self.assertEqual(actual, expected)
+
+if __name__ == "__main__":
+    unittest.main()
+'''
