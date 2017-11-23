7dd56c14d25f295e459fdcdf62cef61287b751eb
HarryFray <harry.fray7@gmail.com>
2017-11-23 08:51:10 -0800

    solved with recursion!

--- Chap2/2.5 SumLists.py
+++ Chap2/2.5 SumLists.py
@@ -1,2 +1,19 @@
+from NodePractice import UnorderedList
 
+
+''' solved with recursion'''
+def SumLists(LL1,LL2):
+    if LL1 == None:return 0
+    else:return LL1.data + LL2.data + SumLists(LL1.next,LL2.next)*10
+
+
+Alist = UnorderedList()
+Alist.AddMultiple([7,1,5])
+Blist = UnorderedList()
+Blist.AddMultiple([5,9,2])
+
+Alist.viewlinkedlist()
+Blist.viewlinkedlist()
+
+print RSumLists(Alist.head,Blist.head)
 
